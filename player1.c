3e089de1eddfb3de7ec19e39875db814dd1019c1
Dhakshinamoorthysrinivasan <47351637+Dhakshinamoorthysrinivasan@users.noreply.github.com>
2019-02-23 03:02:43 -0800

    Create player1.c

---
+++ player1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+	// your code goes here
+int n,i,m=0;;
+char c[100];
+scanf("%s",&c);
+for(i=0;c[i]!='\0';i++)
+{
+		
+}
+for(m=0;i>=0;i--)
+{
+printf("%c",c[i]);	
+}
+	return 0;
+}
